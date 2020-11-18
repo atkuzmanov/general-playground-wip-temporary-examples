10cd8d0348d7437b1858ffb1e0d832131e92d090
atkuzmanov <atkuzmanov@gmail.com>
2020-11-18 13:27:28 +0200

    Work-In-Progress-WIP-2020-11-18-132728

---
+++ interview-playground/interview-playground-java/osi-interview-java-2020-11-20/src/test/java/org/example/AppTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
