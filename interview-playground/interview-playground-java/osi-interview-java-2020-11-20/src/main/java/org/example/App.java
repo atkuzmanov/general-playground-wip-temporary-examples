7d66eb594cec138af7df678c17c64e441044e620
atkuzmanov <atkuzmanov@gmail.com>
2020-11-20 18:55:24 +0200

    Work-In-Progress-WIP-2020-11-20-185524

--- interview-playground/interview-playground-java/osi-interview-java-2020-11-20/src/main/java/org/example/App.java
+++ interview-playground/interview-playground-java/osi-interview-java-2020-11-20/src/main/java/org/example/App.java
@@ -2,20 +2,20 @@ package org.example;
 
 /**
  * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
- *
+ * <p>
  * public class RootToLeafPathSum {
- *
- *     public static boolean hasPathSum(TreeNode node, int sum) {
- *
- * 	    //TODO for implementation
- *     }
+ * <p>
+ * public static boolean hasPathSum(TreeNode node, int sum) {
+ * <p>
+ * //TODO for implementation
+ * }
  * }
  * Examples:
- *                 10
- *               /    \
- *             8       2
- *            / \      /
- *           3   5   2
+ * 10
+ * /    \
+ * 8       2
+ * / \      /
+ * 3   5   2
  * RootToLeafPathSum.hasPathSum(testNode, 23) should return true;
  * RootToLeafPathSum.hasPathSum(testNode, 21) should return true;
  * RootToLeafPathSum.hasPathSum(testNode, 14) should return true;
@@ -24,43 +24,42 @@ package org.example;
  */
 public class App {
     public static void main(String[] args) {
+        TreeNode tr3 = new TreeNode(3);
 
-        TreeNode tr3 = new TreeNode();
-        tr3.data = 3;
+        TreeNode tr2 = new TreeNode(8);
+        tr2.left = tr3;
 
-        TreeNode tr2 = new TreeNode();
-        tr2.data = 8;
-        tr2.leftChild = tr3;
+        TreeNode tr1 = new TreeNode(10);
+        tr1.left = tr2;
 
-        TreeNode tr1 = new TreeNode();
-        tr1.data = 10;
-        tr1.leftChild = tr2;
-
-
-        System.out.println(hasPathSum(tr1, 21));
-
-
+        System.out.println(hasPathSum(tr1, 23));
     }
 
     public static boolean hasPathSum(TreeNode node, int sum) {
-        if(node == null) {
-            return false;
+        if (node == null) {
+//            return false;
+            return sum == 0;
         }
 
-        if(node.rightChild == null && node.leftChild == null && sum == 0) {
-            return true;
-        } else if(node.rightChild == null && node.leftChild == null && sum < 0) {
-            return false;
-        }
+//        if (node.right == null && node.left == null && sum == 0) {
+//            return true;
+//        } else if (node.right == null && node.left == null && sum < 0) {
+//            return false;
+//        }
 
-        return hasPathSum(node.leftChild, sum-=node.data) || hasPathSum(node.rightChild, sum-=node.data);
+//        sum -= node.data;
+//        return hasPathSum(node.leftChild, sum-=node.data) || hasPathSum(node.rightChild, sum-=node.data);
+        return hasPathSum(node.left, sum - node.data) || hasPathSum(node.right, sum - node.data);
     }
 
 }
 
 class TreeNode {
-
     int data = 0;
-    TreeNode leftChild;
-    TreeNode rightChild;
+    TreeNode left, right;
+
+    TreeNode(int data) {
+        this.data = data;
+        left = right = null;
+    }
 }
