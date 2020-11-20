5f635585b82cfc1057b84ab447a7e75f28a27e0f
atkuzmanov <atkuzmanov@gmail.com>
2020-11-20 15:28:57 +0200

    Work-In-Progress-WIP-2020-11-20-152857

---
+++ interview-playground/interview-playground-java/osi-interview-java-springboot-2020-11-20/src/main/java/com/example/osi_interview_java_springboot_2020_11_20/osiinterviewjavaspringboot20201120/OsiInterviewJavaSpringboot20201120Application.java
@@ -0,0 +1,13 @@
+package com.example.osi_interview_java_springboot_2020_11_20.osiinterviewjavaspringboot20201120;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsiInterviewJavaSpringboot20201120Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsiInterviewJavaSpringboot20201120Application.class, args);
+	}
+
+}
---
+++ interview-playground/interview-playground-java/osi-interview-java-springboot-2020-11-20/src/test/java/com/example/osi_interview_java_springboot_2020_11_20/osiinterviewjavaspringboot20201120/OsiInterviewJavaSpringboot20201120ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.osi_interview_java_springboot_2020_11_20.osiinterviewjavaspringboot20201120;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsiInterviewJavaSpringboot20201120ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
